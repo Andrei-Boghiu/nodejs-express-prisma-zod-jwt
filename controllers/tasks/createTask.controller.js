4eb4983a410ae677a53b23ea63fdbaeed0ded6cc
Andrew <vandrei1408@yahoo.com>
2025-07-29 00:19:12 +0300

    fix: task should be assigned to any user

--- controllers/tasks/createTask.controller.js
+++ controllers/tasks/createTask.controller.js
@@ -3,21 +3,19 @@ const { createTaskSchema } = require("../../validators/task.validator");
 
 module.exports = async (req, res) => {
   try {
-    const { title, projectId } = createTaskSchema.parse(req.body);
-    const userId = req.user.id;
+    const { title, projectId, completed, userId } = createTaskSchema.parse(req.body);
 
     const task = await prisma.task.create({
       data: {
         title,
-        userId,
+        userId: userId || req.user.id,
+        completed: Boolean(completed),
         projectId,
       },
     });
 
     res.status(201).json(task);
   } catch (err) {
-    console.log(err);
-
     if (err.name === "ZodError") {
       return res.status(400).json({ error: err.errors });
     }
