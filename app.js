0c2264bea26ad6a14651b439f662c1ddcc8d1656
Andrew <vandrei1408@yahoo.com>
2025-08-17 20:29:45 +0300

    fix: csrf vulnerability

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
@@ -13,10 +14,15 @@ const commentRoutes = require("./routes/comment.routes");
 
 const rateLimiter = require("./middlewares/rateLimiter.middleware");
 const loggerMiddleware = require("./middlewares/logger.middleware");
+const { attachCsrfToken, doubleCsrfProtection, csrfErrorHandler } = require("./middlewares/csrf.middleware");
+
 const fallbackHandler = require("./utils/fallbackHandler.util");
 
 const corsConfig = require("./configs/cors.config");
+const sessionConfig = require("./configs/session.config");
 const app = express();
+
+app.use(session(sessionConfig));
 
 // middleware
 app.use(loggerMiddleware);
@@ -26,6 +32,12 @@ app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/api/auth/csrf-token", attachCsrfToken, (req, res) => {
+  res.json({ csrfToken: res.locals.csrfToken });
+});
+
+app.use(doubleCsrfProtection);
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -33,7 +45,9 @@ app.use("/api/projects", projectRoutes);
 app.use("/api/milestones", milestoneRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("api/memberships", membershipRoutes);
+app.use("/api/memberships", membershipRoutes);
+
+app.use(csrfErrorHandler);
 
 // fallback route handler
 app.use(fallbackHandler);
